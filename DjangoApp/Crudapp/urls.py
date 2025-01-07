9ff574abe5b764562ba5514e8afd2cf98e1d0183
DevAman11 <aman11newus@gmail.com>
2025-01-07 16:37:16 +0530

    save

--- DjangoApp/Crudapp/urls.py
+++ DjangoApp/Crudapp/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('Home',views.Home),
+
+    path('',views.Home),
     path('Blog',views.Blog),
     path('Contact/',views.Contact),
     path('About/', views.About),
